80e4d337d5661933b14b8a608355d3714a2bc777
Rojojal <juetorgr7@gmail.com>
2024-04-14 19:09:19 -0600

    Envio de correo funcional en contacto

--- src/main/java/com/RefugioAnimal/controller/EnviarCorreoController.java
+++ src/main/java/com/RefugioAnimal/controller/EnviarCorreoController.java
@@ -26,11 +26,22 @@ public class EnviarCorreoController {
     @PostMapping("/enviar")
     public String enviarCorreo(Model model, @RequestParam("correo") String correo) {
         try {
-            // Definir el asunto y el contenido del correo
-            String asunto = "¡Bienvenido!";
-            String contenidoHtml = "Gracias por ingresar tu correo electrónico.";
 
-            // Enviar correo electrónico al usuario
+            String asunto = "¡Gracias por contactarnos!";
+           String contenidoHtml = "<p style=\"color:black;\">¡Es un placer saludarlo y agradecerle por haberse puesto en contacto con nosotros! Valoramos enormemente su interés en nuestra organización y en la posibilidad de adoptar una mascota.</p>"
+                    + "<p style=\"color:black;\">Estamos aquí para ayudarlo en cada paso del proceso de adopción y para proporcionarle toda la información y el apoyo que pueda necesitar. Si hay alguna pregunta adicional que tenga o si necesita más detalles sobre nuestros procedimientos de adopción, no dude en hacérnoslo saber. Estamos aquí para asegurarnos de que su experiencia de adopción sea lo más fácil y gratificante posible.</p>"
+                    + "<p style=\"color:black;\">Además, si desea programar una visita a nuestro refugio para conocer a los adorables animales que tenemos disponibles para adopción o para discutir sus necesidades específicas con nuestro equipo de adopciones, estaremos encantados de organizarlo según su conveniencia.</p>"
+                    + "<p style=\"color:black;\">Por favor, siéntase libre de responder a este correo electrónico o de comunicarse con nosotros por teléfono al +506 2222 2222 en cualquier momento. Estamos emocionados de poder ayudarlo en su búsqueda de encontrar a su nuevo amigo peludo.</p>"
+                    + "<p style=\"color:black;\">¡Gracias nuevamente por considerar la adopción y por elegir a Patitas Felices como su destino para encontrar a su compañero de vida!</p>"
+                    + "<p style=\"color:black;\">¡Esperamos tener noticias suyas pronto!</p>"
+                    + "<p style=\"color:black;\">Atentamente,</p>"
+                    + "<p style=\"color:black;\">Patitas Felices</p>"
+                    + "<p style=\"color:black;\">Soporte de Patitas Felices</p>"
+                    + "<p style=\"color:black;\">+506 2222 2222</p>"
+                    + "<p style=\"color:black;\">patitasfelices@gmail.com</p>";
+
+
+
             correoService.enviarCorreoHtml(correo, asunto, contenidoHtml);
 
             model.addAttribute("mensaje", "Correo enviado exitosamente a " + correo);
